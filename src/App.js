c8c3c10a491210dc005b10d68380d7d7736b0eea
Shahid Ameen <iamshahidameen@gmail.com>
2022-12-16 22:20:23 +0500

    Cart Total amount and Total items functionality added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import Navbar from './components/Navbar';
 import CartContainer from './components/CartContainer';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { calculateTotals } from './features/cart/cartSlice';
+// import cartItems from './cartItems';
 
 function App() {
+  const { cartItems } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
   return (
     <main>
       <Navbar />
